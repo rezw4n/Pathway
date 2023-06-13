9db9b2d8d562f5e667e08811ce01c7343ba1e6f8
Rezwan Ahmed <rezwan490@gmail.com>
2023-06-13 11:47:36 +0600

    Added From and To text in the search.

---
+++ custom.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+    let fromLocation = document.querySelector(".directions-icon-depart");
+    fromLocation.classList.remove('directions-icon-depart');
+    fromLocation.innerText = "From";
+
+    let toLocation = document.querySelector(".directions-icon-arrive");
+    toLocation.classList.remove('directions-icon-arrive');
+    toLocation.innerText = "To";
+}, 900)
